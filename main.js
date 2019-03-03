004e8ca83c4635b5e6ec40698a4d26d6d8db486c
overflow326 <s.daizou@outlook.com>
2019-03-03 17:44:53 +0900

    reivew_1

--- main.js
+++ main.js
@@ -4,19 +4,12 @@
  *     - 「JSで変更した後のタイトル」というテキストをセットする
  */
 
-const titleElement = document.getElementById(`title`);
-titleElement.textContent = `JSで変更した後のタイトル`;
-
 /**
  * 課題2: p要素でid属性値が `description` のテキスト内容を変更する
  *   - innerHTMLを使う
  *     - 「JSで変更した後の説明文」というテキストをセットする
  *   - strong要素を使って「説明文」を強調する
  */
-
-const descriptionElement = document.getElementById(`description`);
-descriptionElement.innerHTML = `JSで変更した後の<b>説明文</b>`;
-
 
 /**
  * 課題3: img要素でid属性値が 'profile' のsrc属性値を変更する
@@ -25,13 +18,8 @@ descriptionElement.innerHTML = `JSで変更した後の<b>説明文</b>`;
  *     - 「https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg」を使う
  */
 
-const profileElement = document.getElementById(`profile`);
-profileElement.setAttribute(`src`, `https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg`);
-
 /**
  * 課題4: 課題1で作成したh1要素のDOMにクラス属性値をセットする
  *   - セットするクラス属性値はstyle.cssの中身を参考にする
  *     - 背景色がh1要素の背景色が黄色になればOKです
  */
-
-titleElement.className = `bg-yellow`;
