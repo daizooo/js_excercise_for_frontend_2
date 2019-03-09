9ba1d01f3038608e2fe96a82a51f7c474ca78426
overflow326 <s.daizou@outlook.com>
2019-03-09 22:34:28 +0900

    review_4

--- review_4.js
+++ review_4.js
@@ -2,10 +2,10 @@ const titleElement = document.getElementById('title');
 titleElement.textContent = 'JSで変更した後のタイトル';
 
 const descriptionElement = document.getElementById('description');
-console.log(descriptionElement);
+descriptionElement.innerHTML = 'JSで変更した後の<b>説明文</b>';
 
-const profilrElement = document.getElementById('profile');
-profilrElement.setAttribute(
+const profileElement = document.getElementById('profile');
+profileElement.setAttribute(
   'src',
   'https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg'
 );
